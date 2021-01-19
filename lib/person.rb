83351987a81c9f156098f27fe3561df85ca4ab43
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-19 01:38:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,18 @@
 class Person
-def name
-  @name
+  def name
+    @name
+  end
 
-end
+  def name=(name)
+    @name = name
+  end
 
-def name=(name)
-  @name = name
+  def job
+    @job
+  end
 
-end
-
-def job
-  @job
-
-end
-
-def job=(job)
-  @job = job
-
-end
+  def job=(job)
+    @job = job
+  end
 
 end
